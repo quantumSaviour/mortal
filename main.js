caf9f875d7f39a9795691db063f179662f00a568
quantumSaviour <53609456+QuantumMaster-art@users.noreply.github.com>
2022-09-21 22:44:36 +0300

    Homework-2 done

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const shaokhan = {
+const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+const player1 = {
+	player: 1,
 	name: 'Shao Khan'.toUpperCase(),
-	hp: 80,
+	hp: 100,
 	img: 'https://www.fightersgeneration.com/np3/char/char/gifus/shaokahn-mk3-stance.gif',
 	weapon: ['Frozen Spike', 'Laser Annihilator', 'Molecular desintegrator'],
 	attack: function(name) {
@@ -8,10 +11,10 @@ const shaokhan = {
 	}
 }
 
-
-const scorp = {
+const player2 = {
+	player: 2,
 	name: 'Scorpion'.toUpperCase(),
-	hp: 50,
+	hp: 90,
 	img: 'https://i.gifer.com/Ymo8.gif',
 	weapon: ['Plasma Blaster', 'Wooden stick', 'Sword of Destructious power'],
 	attack: function(name) {
@@ -19,32 +22,24 @@ const scorp = {
 	}
 };
 
-const subzero = {
-	name: 'Sub-zero'.toUpperCase(),
-	hp: 80,
-	img: 'https://www.fightersgeneration.com/characters3/subzero-stance.gif',
-	weapon: ['Frozen Spike', 'Laser Annihilator', 'Molecular desintegrator'],
-	attack: function(name) {
-		console.log(name + ' Fight...');
+function createElement(tag, className) {
+	const $tag = document.createElement(tag);
+	if(className) {
+		$tag.classList.add(className);
 	}
+
+	return $tag;
 }
 
-const createPlayer = (playerClass, playerObj) => {
+const createPlayer = (playerObj) => {
 	/* Creating all needed DOM parts */
-	const $arenas = document.querySelector('.arenas');
-	const $playerDiv = document.createElement('div');
-	const $progressBar = document.createElement('div');
-	const $characterDiv = document.createElement('div');
-	const $lifeDiv = document.createElement('div');
-	const $nameDiv = document.createElement('div');
-	const $charImg = document.createElement('img');
-
-	/* Give all the DOM parts classes */
-	$playerDiv.classList.add(playerClass);
-	$progressBar.classList.add('progressbar');
-	$characterDiv.classList.add('character');
-	$lifeDiv.classList.add('life');
-	$nameDiv.classList.add('name');
+	const $playerDiv = createElement('div', 'player' + playerObj.player);
+	const $progressBar = createElement('div','progressbar');
+	const $characterDiv = createElement('div','character');
+	const $lifeDiv = createElement('div','life');
+	const $nameDiv = createElement('div','name');
+	const $charImg = createElement('img');
+	
 
 	/* Append parts to the divs */
 	$playerDiv.appendChild($progressBar);
@@ -58,9 +53,30 @@ const createPlayer = (playerClass, playerObj) => {
 	$nameDiv.textContent = playerObj.name;
 	$charImg.src = playerObj.img;
 
-	$arenas.appendChild($playerDiv);
-
+	return $playerDiv;
 }
 
-createPlayer('player1', scorp);
-createPlayer('player2', subzero);
+function changeHP(player) {
+	const $playerLife = document.querySelector('.player' + player.player + ' .life');
+	player.hp -= 20;
+	$playerLife.style.width = player.hp + '%';
+
+	if(player.hp < 0) {
+		$arenas.appendChild(playerLose(player.name))
+	}
+}
+
+function playerLose(name) {
+	const $loseTitle = createElement('div', 'loseTitle');
+	$loseTitle.innerText = name + ' lose';
+
+	return $loseTitle;
+}
+
+$randomButton.addEventListener('click', function() {
+	changeHP(player1);
+	changeHP(player2);
+});
+
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
